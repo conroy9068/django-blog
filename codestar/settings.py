aa3b8dab5060e49d20da89aae02738ddda60e43e
David <david@trave.io>
2023-07-30 14:02:41 +0100

    profile update

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['conroy-blog-b81a843c4fe0.herokuapp.com', 'localhost', '127.0.0.1', 'https://conroy-blog-b81a843c4fe0.herokuapp.com/']
+ALLOWED_HOSTS = ['conroy-blog-b81a843c4fe0.herokuapp.com', 'localhost', '127.0.0.1', 'https://conroy-blog-b81a843c4fe0.herokuapp.com']
 
 
 # Application definition
